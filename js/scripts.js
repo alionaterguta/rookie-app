05fb5fca328b989768ca34f440293946efaba839
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2023-12-13 14:57:54 +0100

    cleared the js file and created an array: pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-alert('Hello World');
-let favoriteFood = 'Ratatouille';
-document.write(favoriteFood);
-let favoriteDrink = 'Cappuccino';
-document.write(favoriteDrink);
+let pokemonList = [
+    { name: 'Ivysaur', height: 3.03, type:['Grass','Poison']},
+    { name: 'Charmeleon', height: 3.07, type:['Fire']},
+    { name:'Blastoise', height: 5.03, type:['Water']} ,
+    { name:'Metapod', height: 2.04, type:['Bug']},
+    { name:'Pikachu', height: 1.04, type:['Electric']}
+]; 
+
