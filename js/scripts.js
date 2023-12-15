6d54d51df5f2840d3b803b9929093bd44493744b
alionaterguta <71376066+alionaterguta@users.noreply.github.com>
2023-12-15 13:24:26 +0100

    added a for loop with a conditional
    the for loop will print the name and height of  pokemonList objects  and will add a message if the condition is met: height >5.

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ let pokemonList = [
     { name:'Pikachu', height: 1.04, type:['Electric']}
 ]; 
 
+// print the list of names from pokemonList and coresponding height of that name 
+for (let i =0; i<pokemonList.length; i++) 
+    // print a message if the height is bigger than 5 
+    if (pokemonList[i].height > 5) {
+        document.write('<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + ' Wow, that\'s big!; </p>');
+    } else {
+        document.write( '<p>', pokemonList[i].name + ' - ' + pokemonList[i].height + '; </p>');
+    }
+
+
